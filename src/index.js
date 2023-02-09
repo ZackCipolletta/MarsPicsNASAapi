764a6a79e47d00f46f14636d2b848af05c6f8ecf
Mike <ZackCipolletta@gmail.com>
2023-02-08 16:07:14 -0800

    added further UI styling, will add first 5 images to page, added dropdown to select camera.
    Co-authored-by: NicksFed <nick.guzy76@gmail.com>

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import MarsImages from './js/MarsImages.js';
 
 async function getMarsImages(date) {
   const response = await MarsImages.getImages(date);
-  if (response.photos.length > 1) {
+  if (response.photos) {
     displayImages(response, date);
   } else {
     displayError(response, date);
@@ -15,40 +15,52 @@ async function getMarsImages(date) {
 }
 
 // UI Logic
-
-function displayImages(response) {
+function displayImages(response, date) {
   console.log("hello");
 
-  // <img src="" alt=""></img>;
+  document.getElementById('images').innerText = `Here are the images for ${date} on Mars.`;
 
-                                                                      
-  let image1 = `${response['photos'][0]['img_src']}`;
-  // let image2 = `${response['photos'][1]['img_src']} Hello ${date}`;
-  // let image3 = `${response['photos'][2]['img_src']} Hello ${date}`;
-  // let image4 = `${response['photos'][3]['img_src']} Hello ${date}`;
-  // let image5 = `${response['photos'][4]['img_src']} Hello ${date}`;
-  // let image6 = `${response['photos'][5]['img_src']} Hello ${date}`;
+  response.photos.forEach((element, i) => {
+    if (i <= 5) {
+      let img = document.createElement('img');
+      img.setAttribute('src', `${response['photos'][i]['img_src']}`);
+      document.getElementById('images').append(img);
+      console.log(element);
+    }
+  });
 
-
-  let img = document.createElement('img');
-  img.setAttribute('src', image1);
   
-  document.getElementById('images').append(img);
-    
+
+  // let image1 = `${response['photos'][0]['img_src']}`;
+
+
+  // let img = document.createElement('img');
+  // img.setAttribute('src', image1);
+
+  // document.getElementById('images').append(img);
+
 }
+
 
 function displayError(error, date) {
   document.getElementById('error').innerText = `There was an error getting images from ${date}: ${error}.`;
 }
 
-function marsFrom(event) {
+function marsForm(event) {
   event.preventDefault();
   let date = document.getElementById("date").value;
   console.log(date);
   getMarsImages(date);
 }
 
+function cameraSelect(event) {
+  event.preventDefault();
+  let camera = document.getElementById("cameraSelection").value;
+  console.log(camera);
+}
+
 window.addEventListener("load", function () {
-  document.getElementById('dateSelection').addEventListener("submit", marsFrom);
+  document.getElementById('dateSelection').addEventListener("submit", marsForm);
+  document.getElementById('cameraSelection').addEventListener("submit", cameraSelect);
 
 });
